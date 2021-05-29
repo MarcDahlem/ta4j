570e4f1296042c3a91185a4e442f99962d26b4de
Marc Dahlem <m_dahlem@cs.uni-kl.de>
2021-05-29 14:24:47 +0200

    Simulate not all bars available for pivot calculation

--- ta4j-examples/src/test/java/ta4jexamples/strategies/intelligenthelper/MovingPivotPointIndicator.java
+++ ta4j-examples/src/test/java/ta4jexamples/strategies/intelligenthelper/MovingPivotPointIndicator.java
@@ -12,6 +12,7 @@ import org.ta4j.core.num.Num;
 public abstract class MovingPivotPointIndicator extends CachedIndicator<Num> {
 
     private final int frameSize;
+    private int latestSeenIndex;
 
     protected MovingPivotPointIndicator(BarSeries series, int frameSize) {
         super(series);
@@ -20,24 +21,26 @@ public abstract class MovingPivotPointIndicator extends CachedIndicator<Num> {
 
     @Override
     public Num getValue(int index) {
-        if(index-frameSize >= getBarSeries().getBeginIndex() && index+frameSize < getBarSeries().getEndIndex()) {
-            return super.getValue(index);
-        }
-        return calculate(index);
+        latestSeenIndex = Math.max(latestSeenIndex, index);
+        return calculate(index, latestSeenIndex);
     }
 
     @Override
     protected Num calculate(int index) {
-        Optional<Integer> latestPivotIndex = getLatestPivotIndex(index);
+        throw new IllegalArgumentException("Internal calculation for fixed entry index needed");
+    }
+
+    protected Num calculate(int index, int currentCalculationTick) {
+        Optional<Integer> latestPivotIndex = getLatestPivotIndex(index, currentCalculationTick);
         if (latestPivotIndex.isPresent()) {
             return this.getValueIndicator().getValue(latestPivotIndex.get());
         }
         return NaN;
     }
 
-    private Optional<Integer> getLatestPivotIndex(int index) {
+    private Optional<Integer> getLatestPivotIndex(int index, int currentCalculationTick) {
         while (index >= getBarSeries().getBeginIndex()) {
-            if (isPivotIndex(index)) {
+            if (isPivotIndex(index, currentCalculationTick)) {
                 return Optional.of(index);
             }
             index--;
@@ -45,10 +48,11 @@ public abstract class MovingPivotPointIndicator extends CachedIndicator<Num> {
         return Optional.empty();
     }
 
-    private boolean isPivotIndex(int index) {
+    private boolean isPivotIndex(int index, int currentCalculationTick) {
         Num valueToCheck = getPivotIndicator().getValue(index);
         int startIndex = Math.max(index - frameSize, getBarSeries().getBeginIndex());
-        int endIndex = Math.min(index + frameSize, getBarSeries().getEndIndex());
+        int currentCalculationEndIndex = Math.min(getBarSeries().getEndIndex(), currentCalculationTick);
+        int endIndex = Math.min(index + frameSize, currentCalculationEndIndex);
 
         for (int inFrameIndex = startIndex; inFrameIndex <= endIndex; inFrameIndex++) {
             if (index != inFrameIndex) {
